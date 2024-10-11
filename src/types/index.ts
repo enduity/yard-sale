f3bce618f69b71b8eea36a00de32b9654ca71b3c
Endrik Einberg <endrik.einberg@gmail.com>
2024-10-11 21:14:42 +0300

    feat(marketplace): Faster queries
     - Fetches first listings without using a Chrome instance
     - Improved cache mechanism and cache hits

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,6 @@
 export type { ListingData } from './listings';
 export type { Listing } from './listings';
 export { ListingSource } from './listings';
+
+// Facebook Marketplace JSON types
+export type { JsonContentType, Edge, Node } from './marketplace_json';
